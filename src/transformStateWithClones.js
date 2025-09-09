2edb5a1de55b00857998a674d1f091b17ec2bd20
Nikita Voloshyn <nikita.voloshyn@qpard.com>
2025-09-09 21:23:47 +0300

    implement state transfer by actions

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const stateCopy = structuredClone(state);
+  const ADD_TYPE = 'addProperties';
+  const REMOVE_TYPE = 'removeProperties';
+  const CLEAR_TYPE = 'clear';
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ADD_TYPE:
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case REMOVE_TYPE:
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case CLEAR_TYPE:
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        throw new Error('Wrong type of action');
+    }
+
+    stateHistory.push({ ...stateCopy });
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
